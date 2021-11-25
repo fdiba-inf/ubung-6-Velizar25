a0bde697e3cead469ab03975618b98d34d18e911
Velizar25 <>
2021-11-25 07:57:37 +0000

    Some message

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,19 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+        int sum = 0, r;
+	      int temp = n;    
+        while(n>0)
+	        {    
+          r = n % 10;   
+          sum = (sum*10)+r;    
+          n = n/10;    
+          }    
+        if(temp==sum)    
+          System.out.println("Palindrome number");
+        else    
+          System.out.println("Not a palindrome number"); 
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,39 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean isPassValid = false;
+
+        do {
+            System.out.println("Enter Password: ");
+            String password = input.nextLine();
+
+            if (password.length() < 8) {
+                continue;
+            }
+
+            boolean allLettersOrDigits = true;
+            int digitCount = 0;
+            for (int index = 0; index < password.length(); index++) {
+                char symbol = password.charAt(index);
+                boolean letterOrDigit = Character.isLetterOrDigit(symbol);
+
+                if (!letterOrDigit) {
+                    allLettersOrDigits = false;
+                    break;
+                }
+
+                boolean isDigit = Character.isDigit(symbol);
+
+                if (isDigit) {
+                    digitCount++;
+                }
+
+                if (allLettersOrDigits && digitCount >=2) {
+                    isPassValid = true;
+                }
+            }
+        } while (!isPassValid);
+
+        System.out.println("Password valid!");     
     }
 }
